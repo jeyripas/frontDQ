1ded45f732fc4fbd040b8537906630db2b037437
emereson <escanorrosa123@gmail.com>
2024-01-20 01:42:48 -0500

    hola

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import ProtectedRoutes from './utils/ProtecteRoutes';
 import Login from './pages/Login';
 import Header from './pages/Header';
